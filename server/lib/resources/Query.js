2312753a48352595b1c22eec89746acdd98ec6ff
BoazR <boaz.rymland@forescout.com>
2020-02-17 18:24:41 +0200

    removing 'lib' and adding it to gitignore שיהיה לדראון עולם

--- server/lib/resources/Query.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.queryDefaultDefs = `
-    type Query {}
-`;
-//# sourceMappingURL=Query.js.map
--- server/lib/resources/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const User_1 = require("./User");
-const Item_1 = require("./Item");
-exports.typeDefs = [
-    User_1.userDefs,
-    Item_1.itemDefs
-];
-//# sourceMappingURL=index.js.map
